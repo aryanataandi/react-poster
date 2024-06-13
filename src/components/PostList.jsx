d652675aa066069bcc532bffac7310d1df689a97
aryanataandi <a.ryan.ata.ap@gmail.com>
2024-06-13 08:52:09 +0700

    feat: add new post component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,18 @@
 import Post from "./Post";
 import classes from "./PostList.module.css";
+import NewPost from "./NewPost";
 
 function PostList() {
   return (
+    <>
+    <NewPost />
     <ul className={ classes.posts }>
       <Post author="Lionel Messi" body="React is awesome!" />
       <Post author="Cristiano Ronaldo" body="Frontend development is awesome!" />
       <Post author="Erling Haaland" body="PHP is awesome!" />
       <Post author="Vinicius Jr." body="Laravel is awesome!" />
     </ul>
+    </>
   );
 }
 
